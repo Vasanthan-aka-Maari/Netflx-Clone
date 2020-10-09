8c9d8aac5d7592ee59a04d54a82398c7cd46e94e
Vasanthan <vasanth2754@gmail.com>
2020-10-09 19:16:10 +0530

    Updates

---
+++ precache-manifest.7e7da28a01783bb71d686e6f6c910d6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "53ad2ea8926674496a23adf39c837495",
+    "url": "/Netflx-Clone/index.html"
+  },
+  {
+    "revision": "aa0f81dc84f44d9a1093",
+    "url": "/Netflx-Clone/static/css/main.36bf96fa.chunk.css"
+  },
+  {
+    "revision": "c505406cb973f0c3cb1b",
+    "url": "/Netflx-Clone/static/js/2.68cc80e1.chunk.js"
+  },
+  {
+    "revision": "894f61387b383e6e2e0543fb120b4656",
+    "url": "/Netflx-Clone/static/js/2.68cc80e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa0f81dc84f44d9a1093",
+    "url": "/Netflx-Clone/static/js/main.0fcb3ce0.chunk.js"
+  },
+  {
+    "revision": "5259c24896a9fea0d363",
+    "url": "/Netflx-Clone/static/js/runtime-main.b8b82332.js"
+  }
+]);
